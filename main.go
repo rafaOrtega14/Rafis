fb24cbb25e91231a5b25fa6d0279cee55d73fcfe
Rafa <rafael.ortega.caceres@hotmail.com>
2020-09-21 20:10:08 +0200

    Deleting unnecesary go rutines and mutex read also

--- main.go
+++ main.go
@@ -14,25 +14,25 @@ type Data struct {
 }
 
 var dataMap = make(map[string]interface{})
-var result = make(chan interface{})
 var lock = sync.RWMutex{}
 
 func getData(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	key := params["key"];
-	go readData(key)
-	data := <- result
+	data := readData(key)
 	json.NewEncoder(w).Encode(data)
 }
 func insertData(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var data Data
 	_ = json.NewDecoder(r.Body).Decode(&data)
-	go writeData(data)
+	writeData(data)
 	json.NewEncoder(w).Encode(&data)
 }
-func readData(key string) {
-	result <- dataMap[key];
+func readData(key string) interface{} {
+	lock.Lock()
+	defer lock.Unlock();
+	return dataMap[key];
 }
 func writeData(data Data) {
 	lock.Lock()
@@ -44,6 +44,6 @@ func main() {
     r.HandleFunc("/getData/{key}", getData).Methods("GET");
 	r.HandleFunc("/insert", insertData).Methods("POST");
 
-	fmt.Printf("Server listening to port 80")
-    http.ListenAndServe(":80", r)
+	fmt.Printf("Server listening to port 5000")
+    http.ListenAndServe(":5000", r)
 }
